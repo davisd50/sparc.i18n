ba7d1fe1055e4d5180caad80b1854e25e9e57efa
David Davis <davisd50@gmail.com>
2016-01-17 20:51:58 -0500

    initial commit

---
+++ sparc/i18n/__init__.py
@@ -0,0 +1,4 @@
+# For now, we'll simply use the ZCA i18n system.  If, in the future we decide
+# to change this...we should be able to re-implement without cascading code
+# changes
+from zope.i18nmessageid import MessageFactory as SparcMessageFactory
